c44cc78f3c911988212f315f3c2dcb881a49c438
Salvador Rodriguez <chava.crb@gmail.com>
2024-11-17 16:49:14 -0600

    Setup model file

--- Dgrah/model.py
+++ Dgrah/model.py
@@ -1,3 +1,21 @@
 import pydgraph
 import json
+import data_parser
 
+def create_schema(client):
+    schema = data_parser.CSV_Parser(client=client)
+    schema.load_schema()
+    
+    
+def create_data(client):
+    data = data_parser.CSV_Parser(client=client)
+    data.load_data()
+
+
+def drop_all(client):
+    drop = data_parser.CSV_Parser(client=client)
+    drop.drop_all()
+    
+"""
+    QUERY METHODS ACCORDING TO APP FUNCTIONALITY
+"""
