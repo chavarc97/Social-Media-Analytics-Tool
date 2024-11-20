a6dce7aa60b6b6ee8c94528e3c75dd3beacd893b
Salvador Rodriguez <chava.crb@gmail.com>
2024-11-20 15:07:48 -0600

    Add: implement clear_screen function and enhance main menu with query options

--- Dgrah/client.py
+++ Dgrah/client.py
@@ -28,14 +28,37 @@ def close_client_stub(client_stub):
     client_stub.close()
     
     
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+        
+    
 def main():
     # Init Client Stub and Dgraph Client
     client_stub = create_client_stub()
     client = create_client(client_stub)
     
     # create schema
-    
+    clear_screen()
+    print_menu()
     # menu loop
+    while True:
+        option = int(input('Enter option: '))
+        if option == 1:
+            model.create_data(client)
+        elif option == 2:
+            model.query_data(client)
+        elif option == 3:
+            model.delete_data(client)
+        elif option == 4:
+            model.drop_all(client)
+        elif option == 5:
+            model.drop_all(client)
+            close_client_stub(client_stub)
+            exit(0)
+        else:
+            print('Invalid option')
+            clear_screen()
+            print_menu()
     
     
 if __name__ == '__main__':
